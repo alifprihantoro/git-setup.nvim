36009602905f8a5cab33dc27e423267403ec7202
alifprihantoro <alifprihantoro@gmail.com>
2024-04-30 03:40:49 +0700

    add deps and fix bug

--- lua/muryp-git-setup/plugin.lua
+++ lua/muryp-git-setup/plugin.lua
@@ -1,4 +1,19 @@
 return {
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require "octo".setup({
+        suppress_missing_scope = {
+          projects_v2 = true, -- WARNING: don't remove, this for fix bug
+        }
+      })
+    end
+  },
   'nvim-telescope/telescope.nvim',
   'folke/which-key.nvim',
   'muryp/muryp-gh.nvim',
